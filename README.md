137082d680d7637fe0ae540ee182d0d8ea41cd9f
billymane <bilalkozak.webdev@gmail.com>
2020-09-03 15:15:38 -0400

    Created the Etch-a-Sketch project.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+Created the Etch-a-Sketch project 
+
+https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project
---
+++ script.js
@@ -0,0 +1,40 @@
+const container = document.getElementById("container");
+const resetBtn = document.getElementById("resetBtn");
+
+
+function mouseover() {
+    const gridItem = document.querySelectorAll(".grid-item");
+    gridItem.forEach((item) => {
+        item.addEventListener("mouseover", () => {
+            item.classList.toggle("hover");
+        })
+    })
+}
+
+// function rmGridItem() {
+//     gridItem.forEach((item) => {
+//         item.classList.remove("hover");
+//     })
+// }
+
+function makeRows(rows, cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = "G"
+    container.appendChild(cell).className = "grid-item";
+  };
+};
+
+makeRows(16, 16);
+mouseover();
+
+resetBtn.addEventListener("click", function() {
+    let squares = parseInt(prompt("How many squares per side?"))
+    console.log(squares);
+    container.innerHTML = ""
+    makeRows(squares, squares);
+    mouseover();
+})
+
