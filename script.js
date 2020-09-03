cbdd949cc938ef7cb21f767c970c086c52220576
billymane <bilalkozak.webdev@gmail.com>
2020-09-03 15:21:15 -0400

    Changed sizing in css and removed 'G'

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ function makeRows(rows, cols) {
   container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = "G"
     container.appendChild(cell).className = "grid-item";
   };
 };
